210c8114c1107d48b40cbf0fd31ecdfa63fa2859
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2022-04-20 10:54:39 +0300

    08-assignment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFliter";
 import Card from '../UI/Card'
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
