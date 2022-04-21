8c978b70399a7cd876f5cc509f4f7de3db8160ae
Gardo Kirsipuu <gardo.kirsipuu@khk.ee>
2022-04-21 10:56:05 +0300

    rendering conditionals assignment 2

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,7 +17,7 @@ const ExpenseForm = (props) => {
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value)
-    }
+    };
 
     const submitHandler = (event) => {
         event.preventDefault();
@@ -51,6 +51,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
